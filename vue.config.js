73978487359cbbd7cac4d7bc82cd15c3e8ee6488
gxc <1003823149@qq.com>
2022-10-08 17:14:03 +0800

    change dest

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 // const { configFunction } = require('@vue/cli-service')
 
 module.exports = {
+    base: 'shop',
     publicPath: './',
-    outputDir: 'dist',
+    outputDir: '../aigouzz.github.io/shop',
     chainWebpack: config => {
         console.log(process.env.NODE_ENV);
-    }
+    },
+
 };
