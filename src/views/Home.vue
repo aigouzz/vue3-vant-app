b8729ede1b4c11271333b065ea3542b184b5bd3d
gxc <1003823149@qq.com>
2022-10-15 18:04:00 +0800

    fix:change word

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <header class="home-header wrap" :class="{'active' : headerScroll}">
       <router-link tag="i" to="./category"><i class="nbicon nbmenu2"></i></router-link>
       <div class="header-search">
-        <span class="app-name">新蜂商城</span>
+        <span class="app-name">新品商城</span>
         <i class="iconfont icon-search"></i>
         <router-link tag="span" class="search-title" to="./product-list?from=home">山河无恙，人间皆安</router-link>
       </div>
@@ -93,11 +93,11 @@ export default {
       recommends: [],
       categoryList: [
         {
-          name: '新蜂超市',
+          name: '新品超市',
           imgUrl: 'https://s.yezgea02.com/1604041127880/%E8%B6%85%E5%B8%82%402x.png',
           categoryId: 100001
         }, {
-          name: '新蜂服饰',
+          name: '新品服饰',
           imgUrl: 'https://s.yezgea02.com/1604041127880/%E6%9C%8D%E9%A5%B0%402x.png',
           categoryId: 100003
         }, {
@@ -105,11 +105,11 @@ export default {
           imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%85%A8%E7%90%83%E8%B4%AD%402x.png',
           categoryId: 100002
         }, {
-          name: '新蜂生鲜',
+          name: '新品生鲜',
           imgUrl: 'https://s.yezgea02.com/1604041127880/%E7%94%9F%E9%B2%9C%402x.png',
           categoryId: 100004
         }, {
-          name: '新蜂到家',
+          name: '新品到家',
           imgUrl: 'https://s.yezgea02.com/1604041127880/%E5%88%B0%E5%AE%B6%402x.png',
           categoryId: 100005
         }, {
